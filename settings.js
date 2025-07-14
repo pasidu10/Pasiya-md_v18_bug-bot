c23b3e2842067a4e7cfa70100b4e6b1c56fe7e72
pasidu10 <punnyanamda.thero@gmail.com>
2025-07-14 23:17:36 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,188 @@
+/*
+
+ * Created : Kyy
+ * Base Ori : Eriza
+ * NoWa : 6288286624778
+ * Myt : @KyyXdz
+ 
+ * Hargai Pengembang Dengan Cara Tidak Menghapue Credit Untuk Yang Upload Di Tambahan Tag Yt Atas Dan Rename Juga Untuk Menghargai Pengembang Dengan Cara Tidak Menghapus Credit
+
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ SETTINGS OWNER ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ \\
+global.owner = ["628812023752"] // Ganti No Owner Ada Di Database/owner.json
+global.ownername = "Risky Hosting"
+global.website = "https://xnxx.com/"
+global.idch = ['120363393264886498@newsletter'] 
+global.idch2 = ['120363420479394526@newsletter']
+global.ceha = "https://whatsapp.com/channel/0029VakX8k0KGGGGka6PFN00"
+global.yt = "https://www.youtube.com/@risky_hosting"
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ SETTINGS BOT ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+global.typemenu = 'v1'
+global.namabot = "Angel Multi Device"
+global.botkapital = "ANGEL"
+global.baileys = "@ᴡʜɪsᴋᴇʏsᴏᴄᴋᴇᴛs"
+global.autoread = true
+global.autotyping = true
+global.autobio = false
+global.nomorbot = "628812023752"
+global.version = "5.5.0"
+global.linkgc = "https://chat.whatsapp.com/ImntQ24I4roJCSxtBqakvJ"
+global.packname = 'Botz By'
+global.author = 'Risky\nAngel'
+global.foother = 'Created By Risky'
+global.dana = "isi"
+global.ovo = ""
+global.gopay = ""
+global.wlcm = []
+global.wlcmm = []
+
+global.thumb = fs.readFileSync('./media/kame.jpg'); 
+global.thumbUrl = fs.readFileSync('./media/akame.jpg');
+global.warn = "https://cloudkuimages.guru/uploads/images/6829f8a9a74ec.jpg"
+global.qris = "https://files.catbox.moe/r7whcq.jpg"
+global.thumblist = "https://img1.pixhost.to/images/5640/596847853_ikyjs.jpg"
+global.ftallmenu = "https://files.catbox.moe/cl9xbc.jpg"
+global.thumnreply = "https://files.catbox.moe/b2a5ky.jpg"
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ SETTINGS CPANELV2 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ \\
+global.panel = 'https://'; // Link Panel Kamu
+global.cred = ''; // API PTLA Kamu
+global.apiuser = ''; // API PTLC Kamu
+global.eggs = '15', // Eggs Number (Recommended)
+global.nets = '5', // Nets Number (Recommended)
+global.location = '1', // Location Number (Recommended)
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ SETTINGS LINODE ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ \\
+global.apilinode = ''
+global.apiDigitalOcean = "-"
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ SETTINGS API ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ \\
+global.qriscode = "-"
+global.kyyapi = "KyuuuWoi"
+global.merchan = "OK2334420"
+global.keyorkut = "-"
+
+global.merchantIdOrderKuota = "OK2334420"
+global.apiOrderKuota = "-"
+global.qrisOrderKuota = "-"
+
+global.nekorin = "https://api.nekorinn.my.id"
+global.velyn = "https://velyn.biz.id"
+global.fastres = "https://fastrestapis.fasturl.cloud"
+global.vreden = "https://api.vreden.web.id"
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ SETTINGS RPG ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ \\
+global.rpg = {
+emoticon(string) {
+string = string.toLowerCase()
+let emot = {
+level: '📊',
+limit: '🎫',
+health: '❤️',
+exp: '✨',
+atm: '💳',
+money: '💰',
+bank: '🏦',
+potion: '🥤',
+diamond: '💎',
+common: '📦',
+uncommon: '🛍️',
+mythic: '🎁',
+legendary: '🗃️',
+superior: '💼',
+pet: '🔖',
+trash: '🗑',
+armor: '🥼',
+sword: '⚔️',
+pickaxe: '⛏️',
+fishingrod: '🎣',
+wood: '🪵',
+rock: '🪨',
+string: '🕸️',
+horse: '🐴',
+cat: '🐱',
+dog: '🐶',
+fox: '🦊',
+robo: '🤖',
+petfood: '🍖',
+iron: '⛓️',
+gold: '🪙',
+emerald: '❇️',
+upgrader: '🧰',
+bibitanggur: '🌱',
+bibitjeruk: '🌿',
+bibitapel: '☘️',
+bibitmangga: '🍀',
+bibitpisang: '🌴',
+anggur: '🍇',
+jeruk: '🍊',
+apel: '🍎',
+mangga: '🥭',
+pisang: '🍌',
+botol: '🍾',
+kardus: '📦',
+kaleng: '🏮',
+plastik: '📜',
+gelas: '🧋',
+chip: '♋',
+umpan: '🪱',
+skata: '🧩'
+}
+let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+if (!results.length) return ''
+else return emot[results[0][0]]
+}
+}
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ SETTINGS LIMITZ ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+global.limitawal = {
+    premium: "Infinity",
+    free: 50
+}
+
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ SETTINGS GAME ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ //
+global.gamewaktu = 60 // Game waktu
+global.suit = {};
+global.tictactoe = {};
+global.petakbom = {};
+global.kuis = {};
+global.siapakahaku = {};
+global.asahotak = {};
+global.susunkata = {};
+global.caklontong = {};
+global.family100 = {};
+global.tebaklirik = {};
+global.tebaklagu = {};
+global.tebakgambar2 = {};
+global.tebakkimia = {};
+global.tebakkata = {};
+global.tebakkalimat = {};
+global.tebakbendera = {};
+global.tebakanime = {};
+global.kuismath = {};
+
+global.mess = {
+    success: 'sᴜᴄᴄᴇssғᴜʟʏ',
+    admin: '[ !! ] *sʏsᴛᴇᴍ*\nᴋʜᴜsᴜs ᴀᴅᴍɪɴ ɢʀᴏᴜᴘ',
+    botAdmin: '[ !! ] *sʏsᴛᴇᴍ*\nᴀᴋᴀᴍᴇ ʙᴇʟᴜᴍ ᴊᴀᴅɪ ᴀᴅᴍɪɴ',
+    creator: '[ !! ] *sʏsᴛᴇᴍ*\nғᴇᴀᴛᴜʀᴇ ɪɴɪ ᴋʜᴜsᴜs ᴏᴡɴᴇʀ',
+    group: '[ !! ] *sʏsᴛᴇᴍ*\nғᴇᴀᴛᴜʀᴇ ɪɴɪ ᴋʜᴜsᴜs ɢʀᴏᴜᴘ ᴀᴊᴀ',
+    private: '[ !! ] *sʏsᴛᴇᴍ*\nғᴇᴀᴛᴜʀᴇ ᴋʜᴜsᴜs ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ ᴀᴋᴀᴍᴇ',
+    wait: '[ !! ] *sʏsᴛᴇᴍ*\nᴡᴀɪᴛ ᴀᴋᴀᴍᴇ ᴘʀᴏsᴇs ᴅᴜʟᴜ',
+    notregist: 'ᴋᴀᴍᴜ ʙᴇʟᴜᴍ ᴛᴇʀᴅᴀғᴛᴀʀ ᴅɪ ᴅᴀᴛᴀʙᴀsᴇ ᴀᴋᴀᴍᴇ sɪʟᴀʜᴋᴀɴ ᴅᴀғᴛᴀʀ ᴅᴇɴɢᴀɴ ᴄᴀʀᴀ\n\n*[ ᴅᴀғᴛᴀʀ ᴍᴀɴᴜᴀʟ ]*\n.ᴅᴀғᴛᴀʀ ɴᴀᴍᴀ,ᴜᴍᴜʀ',
+    premium: '[ !! ] *sʏsᴛᴇᴍ*\nғᴇᴀᴛᴜʀᴇ ᴋʜᴜsᴜs ᴘʀᴇᴍɪᴜᴍ ᴀᴋᴀᴍᴇ',
+    endLimit: '[ !! ] *sʏsᴛᴇᴍ*\nʟɪᴍɪᴛ ᴀɴᴅᴀ ʜᴀʙɪs ,, ᴀᴋᴀɴ ᴅɪ ʀᴇsᴇᴛ sᴇᴛᴇʟᴀʜ sᴇʜᴀʀɪ',
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update ${__filename}`))
+delete require.cache[file]
+require(file)
+})
